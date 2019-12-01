9f3288db1883db603be5aadfc70ec7e85f6b7884
Viresh Uberoy <vireshuberoy01@gmail.com>
2019-12-01 11:58:20 +0530

    work ffs

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.static("./public/html/"));
 app.use(express.json({ limit: "1mb" }));
 
 app.get("/", (req, res) => {
-  res.redirect("/public/html/index.html");
+  res.redirect("./public/html/index.html");
 })
 
 app.post("/reviewData", (req, res) => {
