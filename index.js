daa9ff2db8192c77a4bd2a5b32de7a66a5445c6f
Viresh Uberoy <vireshuberoy01@gmail.com>
2019-12-01 10:24:28 +0530

    god i hope this works

---
+++ index.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
+
+const app = new express();
+
+let con = mysql.createConnection({
+  host: "localhost",
+  user: "administrator",
+  password: "veryGood",
+  database: "rateYourProfessor"
+});
+
+const port = 8080;
+let reqNo = 1;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static("./public/"));
+app.use(express.static("./public/html/"));
+app.use(express.json({ limit: "1mb" }));
+
+app.post("/reviewData", (req, res) => {
+  console.log("Got a ping! Number: " + reqNo);
+  const data = req.body;
+  let timestamp = Date.now();
+  data.timestamp = timestamp;
+  console.log(data);
+
+  res.json({
+    status: "success",
+    timestamp: timestamp
+  });
+
+  con.connect(err => {
+    if (err) throw err;
+    console.log("Connected to Database");
+  });
+  let sql =
+    "insert into reviews (university_name, campus, faculty_name, review) values (" +
+    " ' " +
+    data.u_name +
+    " ' " +
+    ", " +
+    " ' " +
+    data.c_name +
+    " ' " +
+    ", " +
+    " ' " +
+    data.f_name +
+    " ' " +
+    ", " +
+    " ' " +
+    data.review +
+    " ' " +
+    ")";
+  con.query(sql, (err, res) => {
+    if (err) throw err;
+    console.log("Inserted into database");
+  });
+});
+
+app.get("/receivingReviews", (req, res) => {
+  // con.connect(err => {
+  //   if (err) throw err;
+  //   console.log("Connected to Database");
+
+  console.log("Got a ping! Number: " + reqNo);
+
+  let sql = "select * from reviews";
+  con.query(sql, (err, result, fields) => {
+    if (err) throw err;
+
+    console.log(result);
+    res.json(result);
+  });
+  // });
+});
+
+app.listen(port, () => {
+  console.log(`Listening at port ${port}`);
+});
---
+++ public/js/main.js
@@ -0,0 +1,59 @@
+document.querySelector("#submit").addEventListener("click", async () => {
+  let u_name = document.querySelector("#university-name");
+  let c_name = document.querySelector("#campus-name");
+  let f_name = document.querySelector("#faculty-name");
+  let review = document.querySelector("#review");
+
+  if (!f_name.value || !review.value) {
+    alert("Please enter the required fields");
+    return;
+  } else {
+    u_name = u_name.value || "";
+    c_name = c_name.value || "";
+    f_name = f_name.value || "";
+    review = review.value || "";
+
+    const data = { u_name, c_name, f_name, review };
+    console.log(data);
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    };
+
+    const response = await fetch("/reviewData", options);
+    const json = await response.json();
+    console.log(json);
+
+    document.querySelector("form").reset();
+  }
+});
+
+document
+  .querySelector("#display-reviews")
+  .addEventListener("click", async () => {
+    const data = await fetch("/receivingReviews");
+    const reviewData = await data.json();
+
+    let div;
+    let rev = [];
+    let reviewFinal = {};
+    let revHeadings = ["Serial Number", "University Name", "Campus Name", "Faculty Name", "Review"];
+    for (let i in reviewData) {
+      for (let j in reviewData[i]) {
+        div = document.createElement("div");
+        div.className = "reviews";
+        rev.push(reviewData[i][j]);
+      }
+      for (let j in revHeadings) {
+        reviewFinal[revHeadings[j]] = rev[j];
+      }
+      // console.log(reviewFinal);
+      div.textContent = JSON.stringify(reviewFinal);
+      document.querySelector("#actually-display-reviews").appendChild(div);
+      rev = [];
+    }
+  });
